ace88ed40f5beaa60e5ba4b8e856a5155c38b5a5
Innocent Zenda <zendainnocent@gmail.com>
2021-01-19 21:03:45 +0300

    update settings

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET_KEY')
-SECRET_KEY = '!ii8@hz%geq!=u9vafssxy#jdh7=@wnm#a%!z-xwplku%4ox2$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)

74ffb9a6f3fbd42f31c84530b8a0fb14ad49103a
Innocent Zenda <50820298+ZendaInnocent@users.noreply.github.com>
2021-01-23 11:29:29 +0300

    update database settings (#7)

--- config/settings.py
+++ config/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
+        'HOST': config('DB_HOST', default='127.0.0.1'),
         'PORT': config('DB_PORT', cast=int),
     }
 }

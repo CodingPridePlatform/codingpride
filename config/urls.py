b84440c4a018144be7fae175e1e0355680673dd3
JAYBLA <64840864+JAYBLA@users.noreply.github.com>
2021-01-26 17:16:43 +0300

    Password Reset Functionality (#23)

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')),
+
 ]
--- main/views.py
+++ main/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    myTemplate = 'pages/home.html'
+    myTemplate = 'home.html'
     return render(request, myTemplate, {})

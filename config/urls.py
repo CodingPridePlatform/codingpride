82161e9b7b4174222aecbd549341fcfdb7652a68
JAYBLA <64840864+JAYBLA@users.noreply.github.com>
2021-01-26 15:14:01 +0300

    named the main app from coding pride to main (#18)

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('codingpride.urls')),
+    path('', include('main.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
 ]

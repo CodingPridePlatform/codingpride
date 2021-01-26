8fe0921f499ed21edf2b5fc5aa1562d09f7f2fc3
JAYBLA <jayblaenterprises@gmail.com>
2021-01-26 16:48:06 +0300

    Password Reset Functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,28 @@
 from django.urls import path
-
 from accounts import views
-
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('register/', views.UserRegistrationView.as_view(),
          name='accounts-register'),
+     path('login/',
+         auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('confirm-email/<str:user_id>/<str:token>/',
-         views.ConfirmRegistrationView.as_view(), name='confirm_email'),
+         views.ConfirmRegistrationView.as_view(), name='confirm_email'), 
+
+    path('password_reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
+     name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+     name='password_reset_complete'),
 ]

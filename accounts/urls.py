62d2b92e17bf576063c703a04045ee68d9ca8c57
Innocent Zenda <zendainnocent@gmail.com>
2021-01-26 09:28:15 +0300

    feat: add email confirmation

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('register/', views.UserRegistrationView.as_view(),
          name='accounts-register'),
+    path('confirm-email/<str:user_id>/<str:token>/',
+         views.ConfirmRegistrationView.as_view(), name='confirm-email'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,24 @@
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.core.mail import EmailMessage
+from django.shortcuts import redirect
 from django.template.loader import get_template
 from django.urls import reverse
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
-from django.views.generic import CreateView
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.views.generic import CreateView, View
 
 from . import forms
+from .models import Profile
 from .tokens import account_activation_token_generator
+
+User = get_user_model()
+
+if not settings.DEBUG:
+    BASE_URL = ''
+else:
+    BASE_URL = 'http://127.0.0.1:8000'
 
 
 class UserRegistrationView(CreateView):
@@ -20,7 +32,7 @@ class UserRegistrationView(CreateView):
         # send confirmation email
         token = account_activation_token_generator.make_token(user)
         user_id = urlsafe_base64_encode(force_bytes(user.id))
-        url = 'http://127.0.0.1:8000' + reverse(
+        url = BASE_URL + reverse(
             'accounts:confirm-email',
             kwargs={'user_id': user_id, 'token': token})
         message = get_template(
@@ -33,4 +45,26 @@ class UserRegistrationView(CreateView):
             from_email='noreply@codingpride.com')
         mail.content_subtype = 'html'
         mail.send()
+        messages.success(self.request,
+                         'Please check your email for confimation.')
         return super().form_valid(form)
+
+
+class ConfirmRegistrationView(View):
+    """View for user to confirm registration."""
+
+    def get(self, request, user_id, token):
+        user_id = force_text(urlsafe_base64_decode(user_id))
+
+        user = User.objects.get(pk=user_id)
+
+        if user and account_activation_token_generator.check_token(
+                user, token):
+            user.is_active = True
+            Profile.objects.create(user=user)
+            user.save()
+            messages.success(
+                request, ('Registration completed successful. '
+                          'Please login to continue..'))
+
+        return redirect('accounts:login')

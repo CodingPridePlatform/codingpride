84fcb323da63d72d4c28cc09bb83961dacfae6dd
JAYBLA <64840864+JAYBLA@users.noreply.github.com>
2021-02-10 15:31:02 +0300

    implemented ask question fucntionality (#56)
    * reseted branch
    
    * implemented Asking Question Functionality
    
    * implemented ask question functionality

--- main/views.py
+++ main/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from .forms import QuestionCreateForm
 
 
 # Create your views here.
 def home(request):
     myTemplate = 'home.html'
     return render(request, myTemplate, {})
+
+
+def question(request):
+    myTemplate = "pages/question-create.html"
+    context = {}
+    form = QuestionCreateForm(request.POST or None,)
+    
+    if form.is_valid():
+        form.save()
+        context['success_message'] = "Your Question Has Been Submitted Successfully"
+        form = QuestionCreateForm()
+    context['form'] = form
+    return render(request,myTemplate,context)
+    
+    
+    

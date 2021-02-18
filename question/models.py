f5f1030ab0d1ad621cdc5cb72f6b5c905c9646dd
DevMed <32010856+devmedtz@users.noreply.github.com>
2021-02-18 14:15:53 +0300

    Edit Question (#66)
    * add db setting
    
    * db setting
    
    * change static and template files structure
    
    * hotfix
    
    * modified layout
    
    * edit question

--- question/models.py
+++ question/models.py
@@ -11,8 +11,8 @@ class Question(models.Model):
     date_published = models.DateTimeField(auto_now_add=True, verbose_name="date published")
     date_updated = models.DateTimeField(auto_now=True, verbose_name="date updated")    
     tags = TaggableManager()
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    slug = models.SlugField(blank=True, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+    slug = models.SlugField(blank=True,null=True)
 
     def __str__(self):
         return self.title

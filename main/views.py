c9111bca0a7d5267ffc854cd7523976c52890559
medson <medsonnaftal@gmail.com>
2021-02-18 13:29:34 +0300

    edit question

--- main/views.py
+++ main/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 
+from question.models import Question
+
 
 # Create your views here.
 def home(request):
+    all_questions = Question.objects.all().order_by('-id')
+    context = {
+		'questions': all_questions,
+	}
+
     myTemplate = 'home.html'
-    return render(request, myTemplate, {})
 
-
-    
-    
+    return render(request, myTemplate, context)
     

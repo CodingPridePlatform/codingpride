e4afb9650422685bbe6c0cdf783bc9485ed52036
JAYBLA <jayblaenterprises@gmail.com>
2021-02-22 13:22:52 +0300

    changes

--- question/views.py
+++ question/views.py
@@ -11,51 +11,52 @@ from .models import *
 @login_required
 def create_edit_question(request, id=None):
 
-	user = request.user
+    user = request.user
 
-	if id:
-		obj = get_object_or_404(Question, id=id)
-		if obj.author != user:
-			return HttpResponseForbidden()
-	else:
-		obj = Question(author=user)
+    if id:
+        obj = get_object_or_404(Question, id=id)
+        if obj.author != user:
+            return HttpResponseForbidden()
+    else:
+        obj = Question(author=user)
 
-	if request.POST:
-		form = QuestionCreateForm(request.POST, instance=obj)
+    if request.POST:
+        form = QuestionCreateForm(request.POST, instance=obj)
 
-		if form.is_valid():
-			obj = form.save(commit=False)
-			obj.author_id = user.id
-			obj.save()
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.author_id = user.id
+            obj.save()
 
-			form.save_m2m() #save tags into db
-					
-			messages.success(request, 'Your Question Has Been Submitted Successfully', extra_tags='alert alert-success')
+            form.save_m2m() #save tags into db
+                    
+            messages.success(request, 'Your Question Has Been Submitted Successfully', extra_tags='alert alert-success')
 
-			return redirect(to='question:qn-create')
+            return redirect(to='question:qn-create')
 
-		else:
-			messages.error(request, 'Errors occurred', extra_tags='alert alert-danger')
-	else:
-		form = QuestionCreateForm(instance=obj)
-	
-	context = {
-		'form': form,
-	}
+        else:
+            messages.error(request, 'Errors occurred', extra_tags='alert alert-danger')
+    else:
+        form = QuestionCreateForm(instance=obj)
+    
+    context = {
+        'form': form,
+    }
 
-	template_name = 'pages/question_create.html'
+    template_name = 'pages/question_create.html'
 
-	return render(request, template_name, context=context)
+    return render(request, template_name, context=context)
 
 
 def list_questions(request):
-	all_questions = Question.objects.all().order_by('-id')
-	myTemplate = "pages/list_questions.html"
-	context = {
-		'questions': all_questions,
-	}
-	return render(request, myTemplate, context)
+    all_questions = Question.objects.all().order_by('-id')
+    myTemplate = "pages/list_questions.html"
+    context = {
+        'questions': all_questions,
+    }
+    return render(request, myTemplate, context)
 
 
 class QuestionDetailView(DetailView):
-	model = Question
+    model = Question
+    template_name = 'pages/question_detail.html'

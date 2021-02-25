242d17829ccadd8a1e6fbdaaac7bb2a151a3347a
Innocent Zenda <zendainnocent@gmail.com>
2021-02-25 23:21:54 +0300

    change paths for questions

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,12 @@
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
+    path('', include('question.urls', namespace='question')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('question/', include('question.urls', namespace='question')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

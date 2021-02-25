0767b8f9f05b6f6319d7c538b4ec3ec29267c0af
Innocent Zenda <zendainnocent@gmail.com>
2021-02-25 22:59:58 +0300

    fix: save non-empty slug

--- question/urls.py
+++ question/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', create_edit_question, name='qn-create'),
     path('<int:id>/edit/', create_edit_question, name='edit_question'),
     path('questions-list/', list_questions, name='qn-list'),
-    path('<pk>/', QuestionDetailView.as_view(), name='question-detail'),
+    path('<slug:slug>/', QuestionDetailView.as_view(), name='question-detail'),
     path('save-like', save_question_like, name='qn-like'),
 ]

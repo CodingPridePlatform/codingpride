45a9887881f571d872eced66adfa971d07324880
Innocent Zenda <zendainnocent@gmail.com>
2021-02-25 22:59:58 +0300

    fix: save non-empty slug

--- question/models.py
+++ question/models.py
@@ -3,13 +3,16 @@ from uuid import uuid4
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils.text import slugify
 from taggit.managers import TaggableManager
 
-User =settings.AUTH_USER_MODEL
+User = settings.AUTH_USER_MODEL
+
 
 class Question(models.Model):
-    title = models.CharField(max_length=250,unique=True)
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250)
     description = RichTextUploadingField(blank=False)
     date_published = models.DateTimeField(
         auto_now_add=True, verbose_name="date published")
@@ -19,19 +22,19 @@ class Question(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
         blank=True, null=True)
-    slug = models.SlugField(default="", max_length=250, editable=False)
-
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('question:question-detail', kwargs={'slug': self.slug})
+
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title) + "/" + str(uuid4())
+        self.slug = slugify(self.title) + "-" + str(uuid4())
         return super().save(*args, **kwargs)
 
 
 class QuestionLike(models.Model):
-    question = models.ForeignKey(Question,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='upvote_user')
-
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='upvote_user')

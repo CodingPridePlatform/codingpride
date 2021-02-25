e08d281c5b64fa81004d39107bf89210373ccdb4
Innocent Zenda <50820298+ZendaInnocent@users.noreply.github.com>
2021-02-25 10:38:47 +0300

    show login/logout accordingly (#71)
    * show login/logout accordingly
    
    * enhance accessibility in search bar, login and signup modal
    
    * fix: images locations on templates

--- question/models.py
+++ question/models.py
@@ -1,27 +1,29 @@
-import uuid
 from uuid import uuid4
-from django.db import models
-from django.conf import settings
-from django.utils.text import slugify
-from ckeditor_uploader.fields import RichTextUploadingField
-from taggit.managers import TaggableManager
-from datetime import datetime
 
-# Create your models here.
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.conf import settings
+from django.db import models
+from django.utils.text import slugify
+from taggit.managers import TaggableManager
+
+
 class Question(models.Model):
     title = models.CharField(max_length=250)
     description = RichTextUploadingField(blank=False)
-    date_published = models.DateTimeField(auto_now_add=True, verbose_name="date published")
-    date_updated = models.DateTimeField(auto_now=True, verbose_name="date updated")    
+    date_published = models.DateTimeField(
+        auto_now_add=True, verbose_name="date published")
+    date_updated = models.DateTimeField(
+        auto_now=True, verbose_name="date updated")
     tags = TaggableManager()
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
-    slug = models.SlugField(default="",max_length=250,editable=False)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
+        blank=True, null=True)
+    slug = models.SlugField(default="", max_length=250, editable=False)
 
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title) + "/" + str(uuid.uuid4())
+            self.slug = slugify(self.title) + "/" + str(uuid4())
         return super().save(*args, **kwargs)
-    

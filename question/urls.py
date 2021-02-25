903ea3bd162abec9016b824c0124c38141002902
Innocent Zenda <zendainnocent@gmail.com>
2021-02-25 23:14:47 +0300

    fix: question list and detail functionality

--- question/urls.py
+++ question/urls.py
@@ -5,9 +5,9 @@ from .views import *
 app_name = 'question'
 
 urlpatterns = [
-    path('', create_edit_question, name='qn-create'),
+    path('ask-question/', create_edit_question, name='question-add'),
     path('<int:id>/edit/', create_edit_question, name='edit_question'),
-    path('questions-list/', list_questions, name='qn-list'),
+    path('questions/', question_list_view, name='question-list'),
     path('<slug:slug>/', QuestionDetailView.as_view(), name='question-detail'),
     path('save-like', save_question_like, name='qn-like'),
 ]

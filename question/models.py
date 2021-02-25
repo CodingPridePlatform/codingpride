ff735e2b5428b0838969f81ffb5dc0437ce36896
JAYBLA <64840864+JAYBLA@users.noreply.github.com>
2021-02-25 22:33:56 +0300

    Like and pagination (#72)
    * fixed on login and created profile functionality
    
    * removed unnecessary scripts
    
    * Frontend cont... (#45)
    
    * add db setting
    
    * db setting
    
    * change static and template files structure
    
    * hotfix
    
    * change font style and remove fontface
    
    * frontend pregress
    
    * merged changes
    
    * removed form.py
    
    * pulled layout
    
    * fixed toggle items visibility
    
    * css
    
    * question templates
    
    * implemented codehighlighter
    
    * saved the changes
    
    * changes
    
    * qn-likes
    
    * changes
    
    * Implemented like and pagination
    
    Co-authored-by: DevMed <32010856+devmedtz@users.noreply.github.com>

--- question/models.py
+++ question/models.py
@@ -6,9 +6,10 @@ from django.db import models
 from django.utils.text import slugify
 from taggit.managers import TaggableManager
 
+User =settings.AUTH_USER_MODEL
 
 class Question(models.Model):
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=250,unique=True)
     description = RichTextUploadingField(blank=False)
     date_published = models.DateTimeField(
         auto_now_add=True, verbose_name="date published")
@@ -20,6 +21,7 @@ class Question(models.Model):
         blank=True, null=True)
     slug = models.SlugField(default="", max_length=250, editable=False)
 
+
     def __str__(self):
         return self.title
 
@@ -27,3 +29,9 @@ class Question(models.Model):
         if not self.slug:
             self.slug = slugify(self.title) + "/" + str(uuid4())
         return super().save(*args, **kwargs)
+
+
+class QuestionLike(models.Model):
+    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='upvote_user')
+
--- question/urls.py
+++ question/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<int:id>/edit/', create_edit_question, name='edit_question'),
     path('questions-list/', list_questions, name='qn-list'),
     path('<pk>/', QuestionDetailView.as_view(), name='question-detail'),
+    path('save-like', save_question_like, name='qn-like'),
 ]

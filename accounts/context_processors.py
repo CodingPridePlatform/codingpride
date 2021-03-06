6b5042e3a7d496df77e1e99fdf3043879911efb9
Innocent Zenda <zendainnocent@gmail.com>
2021-03-06 12:54:20 +0300

    remove accounts app, add django-allauth

--- accounts/context_processors.py
+++
@@ -1,8 +0,0 @@
-from . import forms
-
-
-def user_forms_context(request):
-    return {
-        'signin_form': forms.UserLoginForm(),
-        'signup_form': forms.UserCreationForm(),
-    }
--- config/settings.py
+++ config/settings.py
@@ -34,16 +34,13 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1',
 # Application definition
 
 INSTALLED_APPS = [
-    # Custom Apps
-    'accounts.apps.AccountsConfig',
-    'crispy_forms',
     'main',
     'question',
+
     'widget_tweaks',
     'taggit',
     'ckeditor',
     'ckeditor_uploader',
-    'bootstrap_modal_forms',
 
     # Django Apps
     'django.contrib.admin',
@@ -52,8 +49,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize'
+    'django.contrib.humanize',
+    'django.contrib.sites',
 
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
@@ -81,7 +82,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'accounts.context_processors.user_forms_context',
             ],
         },
     },
@@ -151,8 +151,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
 
-AUTH_USER_MODEL = 'accounts.User'
-
 LOGOUT_REDIRECT_URL = '/'
 
 BASE_URL = config('BASE_URL', default='http://127.0.0.1:8000')
@@ -186,3 +184,11 @@ CKEDITOR_CONFIGS = {
         'extraPlugins': 'codesnippet',
     },
 }
+
+# Django Allauth configurations
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1

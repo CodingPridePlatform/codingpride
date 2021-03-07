5e79fea2c79f986be61ddbca235f5eb6c7ce9ea9
Innocent Zenda <zendainnocent@gmail.com>
2021-03-07 09:47:49 +0300

    add about and contact pages

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path, include
+from django.urls import include, path
+from django.views.generic import TemplateView
+
 from .views import *
 
 app_name = 'main'
 
 urlpatterns = [
     path('', home, name='home-page'),
+    path('about/', TemplateView.as_view(template_name='pages/about.html'),
+         name='about'),
+    path('contact/', TemplateView.as_view(template_name='pages/contact.html'),
+         name='contact'),
 ]
